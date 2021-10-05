1128e4e6b98a7326d6aa70a2e756d06faa2f5a0c
LuisAncizarRamirez <luis.ancizar.ramirez@gmail.com>
2021-10-04 21:15:28 -0500

    Creacion un Crud

---
+++ AcmeNotas.App/AcmeNotas.App.Presentacion/Pages/CrudPersona/Index.cshtml.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using AcmeNotas.App.Dominio;
+using AcmeNotas.App.Persistencia;
+
+namespace AcmeNotas.App.Presentacion.Pages.CrudPersona
+{
+    public class IndexModel : PageModel
+    {
+        private readonly AcmeNotas.App.Persistencia.Conexion _context;
+
+        public IndexModel(AcmeNotas.App.Persistencia.Conexion context)
+        {
+            _context = context;
+        }
+
+        public IList<Persona> Persona { get;set; }
+
+        public async Task OnGetAsync()
+        {
+            Persona = await _context.Personas.ToListAsync();
+        }
+    }
+}
